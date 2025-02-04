2c497cb9a056634efc24962b848d1fb555777234
Malik186 <ianmathew186@gmail.com>
2025-02-05 01:16:45 +0300

    updates the file to use redux and handle authentication

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,29 @@
-import { Provider } from "react-redux";
-import store from "../redux/store";
 import "../styles/globals.css";
+import { useEffect } from "react";
+import { Provider, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import store from "../store";
 
-function MyApp({ Component, pageProps }) {
+function AuthGuard({ children }) {
+  const router = useRouter();
+  const userInfo = useSelector((state) => state.auth.userInfo);
+  const authRequired = !["/login", "/register"].includes(router.pathname);
+
+  useEffect(() => {
+    if (authRequired && !userInfo) {
+      router.push("/login"); //Redirect if user is not authenticated
+    }
+  }, [userInfo, authRequired]);
+
+  return children;
+}
+
+export default function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <AuthGuard>
+        <Component {...pageProps} />
+      </AuthGuard>
     </Provider>
   );
 }
-
-export default MyApp;
