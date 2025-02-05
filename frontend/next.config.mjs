e38ae3b90b035998a2c0c1ea6747c7c5b2e72d4a
Malik <120444800+Malik186@users.noreply.github.com>
2025-02-05 23:28:46 +0300

    Updates next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone", // Ensure the correct build output format
+  images: {
+    domains: ["https://savannah-photo-app.onrender.com/"], //  backend domain if using images from API
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
