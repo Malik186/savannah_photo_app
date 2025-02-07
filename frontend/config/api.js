5f8370b65ccdcb6e96a9722ff9bffad065144252
Malik186 <ianmathew186@gmail.com>
2025-02-07 16:45:01 +0300

    updates the api.js

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,2 +1,2 @@
-const API_URL = "https://savannah-photo-app.onrender.com/api";
+const API_URL = "https://savannah-photo-app.onrender.com/api"; // For testing; replace with your actual URI for production
 export default API_URL;
